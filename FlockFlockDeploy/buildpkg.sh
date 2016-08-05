227473adbffda647ab9ead33651a5d72ea44debb
Jonathan Zdziarski <jonz@Pestilence.local>
2016-08-05 10:16:18 -0400

    add syntax and identity

--- FlockFlockDeploy/buildpkg.sh
+++ FlockFlockDeploy/buildpkg.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 BUILD=$1
-pkgbuild --root `pwd`/root --scripts pkgbuild-scripts --identifier com.zdziarski.FlockFlock --version $BUILD --ownership recommended --install-location / --sign "Jonathan Zdziarski" FlockFlock-$BUILD.pkg
 
-#codesign -fs "Mac Developer: Jonathan Zdziarski" FlockFlock-$BUILD.pkg
+if [ "$1x" == "x" ]
+then
+    echo "syntax: $0 [build_number]"
+    exit
+fi
+IDENTITY="Jonathan Zdziarski"
+
+pkgbuild --root `pwd`/root --scripts pkgbuild-scripts --identifier com.zdziarski.FlockFlock --version $BUILD --ownership recommended --install-location / --sign "$IDENTITY" FlockFlock-$BUILD.pkg
