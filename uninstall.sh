f234f4dcc526b0588c90e490009f267b4460b8fb
Jonathan Zdziarski <jonz@Pestilence.local>
2016-08-07 14:38:46 -0400

    updated path to launch agent

--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,4 @@
 sudo rm -rf /Library/Extensions/FlockFlock.kext
 sudo rm -rf /Library/Application\ Support/FlockFlock
 sudo rm -f /Library/LaunchDaemons/com.zdziarski.FlockFlock.plist
-sudo rm -f ~/Library/LaunchAgents/com.zdziarski.FlockFlockUserAgent.plist
+sudo rm -f /Library/LaunchAgents/com.zdziarski.FlockFlockUserAgent.plist
