5f3c58a5588da509bb34d0aaa1b9b7b5210d1fcc
Jonathan Zdziarski <jonz@Pestilence.local>
2016-08-08 15:08:51 -0400

    0.0.19

--- uninstall.sh
+++ uninstall.sh
@@ -2,5 +2,8 @@
 
 sudo rm -rf /Library/Extensions/FlockFlock.kext
 sudo rm -rf /Library/Application\ Support/FlockFlock
+sudo rm -rf /Applications/FlockFlockUserAgent.app
 sudo rm -f /Library/LaunchDaemons/com.zdziarski.FlockFlock.plist
+sudo rm -f /Library/LaunchDaemons/com.zdziarski.FlockFlockDaemon.plist
 sudo rm -f /Library/LaunchAgents/com.zdziarski.FlockFlockUserAgent.plist
+sudo rm -rf /Library/FlockFlock
