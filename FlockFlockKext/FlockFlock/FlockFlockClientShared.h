a0a7db03368a0f273effc9fa698dc7b0012421d6
Jonathan Zdziarski <jonz@Pestilence.local>
2016-08-09 16:22:51 -0400

    0.0.24_persistence_unloadable

--- FlockFlockKext/FlockFlock/FlockFlockClientShared.h
+++ FlockFlockKext/FlockFlock/FlockFlockClientShared.h
@@ -9,8 +9,8 @@
 #ifndef FlockFlockClientShared_h
 #define FlockFlockClientShared_h
 
-// #define PERSISTENCE
-#define FLOCKFLOCK_VERSION  "alpha_0.0.23_persistence"
+#define PERSISTENCE
+#define FLOCKFLOCK_VERSION  "alpha_0.0.24_persistence"
 
 #define DRIVER "com_zdziarski_driver_FlockFlock"
 #define SKEY_LEN 32
